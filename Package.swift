d5431b0140d8904c7f30af534f21f28ca3abd134
Oliver <oliveroneill04@gmail.com>
2018-09-01 19:11:54 +1000

    Update Swift Package Manager to 4.0
    I've also added a Tests directory as I plan to add some tests.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,16 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
-let package = Package(name: "INI")
+let package = Package(
+  name: "INI",
+  products: [
+      .library(name: "INI", targets: ["INI"]),
+  ],
+  targets: [
+      .target(name: "INI"),
+        .testTarget(
+            name: "INITests",
+            dependencies: ["INI"]),
+  ]
+)
---
+++ Tests/INITests/ParseINITests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import INI
+
+final class ParseINITests: XCTestCase {
+}
